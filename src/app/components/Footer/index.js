0c0ba7d68b88194bb5d0975c9aa76217bf5b1c20
khairulharu <khairulaswad719@gmail.com>
2024-08-23 20:36:18 +0800

    add background footer

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -1,6 +1,19 @@
 export default function Footer() {
   return (
     <>
+    <div class="relative isolate overflow-hidden px-6 pt-16 shadow-2xl sm:px-16 md:pt-24 lg:px-24 lg:pt-0">
+     <svg viewBox="0 0 1024 1024"
+         class="absolute left-1/2 top-1 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+         aria-hidden="true">
+         <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+             fill-opacity="0.7" />
+         <defs>
+             <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                 <stop stop-color="#7775D6" />
+                 <stop offset="1" stop-color="#E935C1" />
+             </radialGradient>
+         </defs>
+     </svg>
       <footer class="sm:flex sm:items-center sm:justify-between p-4 sm:p-6 xl:p-8 antialiased">
         <p class="mb-4 text-sm text-center text-gray-500 dark:text-gray-400 sm:mb-0">
           &copy; 2024{" "}
@@ -117,6 +130,7 @@ export default function Footer() {
           </div>
         </div>
       </footer>
+      </div>
     </>
   );
 }
