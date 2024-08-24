af1e91c3dc11d3474e7f611c36f1470fe26dd9ef
khairulharu <khairulaswad719@gmail.com>
2024-08-24 08:45:25 +0800

    add: logo in big

--- src/app/components/Navbar/index.js
+++ src/app/components/Navbar/index.js
@@ -1,9 +1,17 @@
+import Image from "next/image";
+
+
 export default function Navbar() {
   return (
     <>
       <nav>
-        <div className="py-8 text-center flex justify-center text-white gap-1">
-          <p className="font-extrabold sm:text-3xl lg:text-4xl text-4xl tracking-widest">
+        <div className="py-6 text-center flex justify-center text-white gap-1">
+          <Image src={"/WebLogo.png"}
+          width={"50"}
+          height={"100"}
+          className="hidden sm:block"
+          />
+          <p className="font-extrabold sm:text-3xl lg:text-4xl text-4xl tracking-widest grid place-content-center">
             WEBSTUDIOBONE
           </p>
         </div>
